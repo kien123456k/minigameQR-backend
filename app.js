847833eff36eb8fe0b5c93de5dd621361e60354a
Ngoc Huy <huybui479@gmail.com>
2020-09-23 09:09:00 +0700

    fix route spa in express

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 var cors = require('cors');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const path = require('path');
+var publicFolder = path.resolve(__dirname, './public')//make sure you reference the right path
 
 mongoose.connect('mongodb+srv://fcode:suprlBrlI5e4njCu@cluster0.pihma.mongodb.net/minigame?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -19,8 +20,7 @@ db.once('open', async function () {
   console.log(`connected`);
 });
 
-const tokenRouter = require('./routes/token');
-const userRouter = require('./routes/user');
+const apiRouter = require('./routes/');
 
 const app = express();
 app.use(cors());
@@ -28,10 +28,12 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static('public'));
-app.use('/api/', tokenRouter);
-app.use('/api/user', userRouter);
 
+app.use('/', express.static('public'));
+app.use('/api', apiRouter);
+app.all('/*', function(req, res) {
+    res.sendFile('index.html', {root: publicFolder});
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404, 'Not found'));
