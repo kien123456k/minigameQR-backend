470624092e1e21c67e90fbaa1abf9b1bafae4b08
Trung Kien <kien123456k@gmail.com>
2020-09-25 11:05:22 +0700

    resolve uppercase and lowercase problem

--- controllers/user.js
+++ controllers/user.js
@@ -8,8 +8,8 @@ const numberNormalQuestion = 6;
 module.exports = {
   register: (req, res, next) => {
     const token = req.body.token;
-    const name = req.body.name;
-    const studentID = req.body.studentID;
+    const name = req.body.name.toLowerCase();
+    const studentID = req.body.studentID.toUpperCase();
     user.findOne({token: token}, function (err, student1) {
       if (!student1) {
         const date = new Date();
@@ -25,7 +25,7 @@ module.exports = {
           },
         });
       } else if (!student1.studentID) {
-        user.findOne({studentID: studentID.toUpperCase()}, (err, student2) => {
+        user.findOne({studentID: studentID}, (err, student2) => {
           if (student2) {
             const date = new Date();
             res.status(403).json({
@@ -41,7 +41,7 @@ module.exports = {
             });
           } else {
             student1.name = name;
-            student1.studentID = studentID.toUpperCase();
+            student1.studentID = studentID;
             student1.save();
             res.status(200).json({
               success: true,
@@ -50,7 +50,7 @@ module.exports = {
           }
         });
       } else {
-        if (student1.studentID.toUpperCase() !== studentID) {
+        if (student1.studentID !== studentID) {
           const date = new Date();
           res.status(403).json({
             success: false,
@@ -64,7 +64,7 @@ module.exports = {
             },
           });
         } else {
-          if (student1.name.toUpperCase() !== name.toUpperCase()) {
+          if (student1.name !== name) {
             const date = new Date();
             res.status(403).json({
               success: false,
@@ -90,22 +90,107 @@ module.exports = {
 
   start: (req, res, next) => {
     const token = req.query.token;
-    const studentID = req.query.studentID;
-    const name = req.query.name;
-    user.findOne(
-      {token: token, studentID: studentID.toUpperCase(), name: name},
-      function (err, student) {
-        if (!student) {
+    const name = req.query.name.toLowerCase();
+    const studentID = req.query.studentID.toUpperCase();
+    user.findOne({token: token, studentID: studentID, name: name}, (err, student) => {
+      if (!student) {
+        const date = new Date();
+        res.status(400).json({
+          success: false,
+          message: 'Invalid user!',
+          data: {
+            code: 400,
+            timestamp: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+            path: `/start/${token}/${studentID}/${name}`,
+            method: 'GET',
+            message: 'User you request is not found',
+          },
+        });
+      } else {
+        if (student.timeEnd) {
           const date = new Date();
-          res.status(400).json({
+          res.status(403).json({
             success: false,
-            message: 'Invalid user!',
+            message: 'User already submitted!',
             data: {
-              code: 400,
+              code: 403,
               timestamp: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
-              path: `/start/${token}/${studentID}/${name}`,
+              path: `/start?token=${token}&studentID=${studentID}&name=${name}`,
               method: 'GET',
-              message: 'User you request is not found',
+              message: 'User you request already submitted',
+            },
+          });
+        } else {
+          if (student.timeStart) {
+            res.status(200).json({
+              success: true,
+              message: 'Retrieved data successfully!',
+              data: {
+                questions: student.questions,
+                timeStart: student.timeStart,
+                message: 'Retrieved data successfully from database',
+              },
+            });
+          } else {
+            let quizs = [];
+            let p1 = normalQuestion.aggregate().sample(numberNormalQuestion);
+            let p2 = hardQuestion.aggregate().sample(numberHardQuestion);
+            Promise.all([p1, p2]).then(async (values) => {
+              quizs = quizs.concat(values[0]);
+              quizs = quizs.concat(values[1]);
+              student.questions = quizs;
+              student.timeStart = Date.now();
+              await student.save();
+              for (let i of quizs) {
+                delete i.answer;
+              }
+              res.status(200).json({
+                success: true,
+                message: 'Retrieved data successfully!',
+                data: {
+                  questions: student.questions,
+                  timeStart: student.timeStart,
+                  message: 'Retrieved data successfully after generating',
+                },
+              });
+            });
+          }
+        }
+      }
+    });
+  },
+
+  end: (req, res, next) => {
+    const token = req.body.token;
+    const name = req.body.name.toLowerCase();
+    const studentID = req.body.studentID.toUpperCase();
+    const answer = req.body.answer;
+    user.findOne({token: token, name: name, studentID: studentID}, (err, student) => {
+      if (!student) {
+        const date = new Date();
+        res.status(400).json({
+          success: false,
+          message: 'User is invalid!',
+          data: {
+            code: 403,
+            timestamp: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+            path: '/end',
+            method: 'POST',
+            message: 'User you request is not found',
+          },
+        });
+      } else {
+        if (!student.timeStart) {
+          const date = new Date();
+          res.status(403).json({
+            success: false,
+            message: 'User is not start the game!',
+            data: {
+              code: 403,
+              timestamp: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+              path: '/end',
+              method: 'POST',
+              message: 'User you request is not start the game',
             },
           });
         } else {
@@ -113,122 +198,31 @@ module.exports = {
             const date = new Date();
             res.status(403).json({
               success: false,
-              message: 'User already submitted!',
-              data: {
-                code: 403,
-                timestamp: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
-                path: `/start?token=${token}&studentID=${studentID}&name=${name}`,
-                method: 'GET',
-                message: 'User you request already submitted',
-              },
-            });
-          } else {
-            if (student.timeStart) {
-              res.status(200).json({
-                success: true,
-                message: 'Retrieved data successfully!',
-                data: {
-                  questions: student.questions,
-                  timeStart: student.timeStart,
-                  message: 'Retrieved data successfully from database',
-                },
-              });
-            } else {
-              let quizs = [];
-              let p1 = normalQuestion.aggregate().sample(numberNormalQuestion);
-              let p2 = hardQuestion.aggregate().sample(numberHardQuestion);
-              Promise.all([p1, p2]).then(async (values) => {
-                quizs = quizs.concat(values[0]);
-                quizs = quizs.concat(values[1]);
-                student.questions = quizs;
-                student.timeStart = Date.now();
-                await student.save();
-                for (let i of quizs) {
-                  delete i.answer;
-                }
-                res.status(200).json({
-                  success: true,
-                  message: 'Retrieved data successfully!',
-                  data: {
-                    questions: student.questions,
-                    timeStart: student.timeStart,
-                    message: 'Retrieved data successfully after generating',
-                  },
-                });
-              });
-            }
-          }
-        }
-      }
-    );
-  },
-
-  end: (req, res, next) => {
-    const token = req.body.token;
-    const name = req.body.name;
-    const studentID = req.body.studentID;
-    const answer = req.body.answer;
-    user.findOne(
-      {token: token, name: name, studentID: studentID.toUpperCase()},
-      (err, student) => {
-        if (!student) {
-          const date = new Date();
-          res.status(400).json({
-            success: false,
-            message: 'User is invalid!',
-            data: {
-              code: 403,
-              timestamp: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
-              path: '/end',
-              method: 'POST',
-              message: 'User you request is not found',
-            },
-          });
-        } else {
-          if (!student.timeStart) {
-            const date = new Date();
-            res.status(403).json({
-              success: false,
-              message: 'User is not start the game!',
+              message: 'User already finished the game!',
               data: {
                 code: 403,
                 timestamp: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
                 path: '/end',
                 method: 'POST',
-                message: 'User you request is not start the game',
+                message: 'User you request already finished the game',
               },
             });
           } else {
-            if (student.timeEnd) {
-              const date = new Date();
-              res.status(403).json({
-                success: false,
-                message: 'User already finished the game!',
-                data: {
-                  code: 403,
-                  timestamp: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
-                  path: '/end',
-                  method: 'POST',
-                  message: 'User you request already finished the game',
-                },
-              });
-            } else {
-              student.timeEnd = Date.now();
-              let score = 0;
-              for (let i in answer) {
-                if (answer[i] === student.questions[i].answer) score++;
-              }
-              student.score = score;
-              student.time = student.timeEnd - student.timeStart;
-              student.save();
-              res.status(200).json({
-                success: true,
-                message: 'Submit successfully!',
-              });
+            student.timeEnd = Date.now();
+            let score = 0;
+            for (let i in answer) {
+              if (answer[i] === student.questions[i].answer) score++;
             }
+            student.score = score;
+            student.time = student.timeEnd - student.timeStart;
+            student.save();
+            res.status(200).json({
+              success: true,
+              message: 'Submit successfully!',
+            });
           }
         }
       }
-    );
+    });
   },
 };
