6022943bd8d61d0ff2b49125056f12ef12f854ee
Ngoc Huy <huybui479@gmail.com>
2020-09-28 09:59:21 +0700

    update frontend

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d2198123595a286dbbc05bfeae85060.js"
+  "/precache-manifest.8505ae7b58143a34158541dd89d1ddb3.js"
 );
 
 self.addEventListener('message', (event) => {
