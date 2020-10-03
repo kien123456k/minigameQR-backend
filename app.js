c69e6deb1ea0a7515420bab6ee31dd2bf632de19
Trung Kien <kien123456k@gmail.com>
2020-10-04 00:36:52 +0700

    remove redundancy code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 var cors = require('cors');
 require('dotenv').config();
